f52b29900bf5da17640a151fc35a32b509c51d6c
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-04-17 10:21:14 +0200

    store

--- src/App.vue
+++ src/App.vue
@@ -12,3 +12,10 @@
 <style lang="scss" scoped>
 
 </style>
+
+
+
+<!-- api
+bf23213818e51de206214cfbb2cb2c83
+
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjIzMjEzODE4ZTUxZGUyMDYyMTRjZmJiMmNiMmM4MyIsInN1YiI6IjY2MWY4MTFjMjBhZjc3MDE3ZDNmMzRjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9tWQKIYCF5Pw61-cUic7CEEDzaSJg5oKIDXYemzaLrY -->
